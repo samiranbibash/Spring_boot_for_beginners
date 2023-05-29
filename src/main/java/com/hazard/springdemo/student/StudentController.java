d7ea7d8c469319f42c4782c86fc55848c03cd272
Samiran Bibash <samiranbibash2218@gmail.com>
2023-05-29 10:46:25 +0545

    Building the first API

---
+++ src/main/java/com/hazard/springdemo/student/StudentController.java
@@ -0,0 +1,16 @@
+package com.hazard.springdemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findallstudnets(){
+        return List.of("Eden", "Hazard");
+    }
+}
